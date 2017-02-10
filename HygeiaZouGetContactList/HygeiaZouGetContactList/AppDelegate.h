c2102b20aeba420e9db8b716e622916995e7f449
HygeiaZou <2524266795@qq.com>
2017-02-10 11:46:53 +0800

    获取通讯录列表

---
+++ HygeiaZouGetContactList/HygeiaZouGetContactList/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HygeiaZouGetContactList
+//
+//  Created by 海灵 on 17/2/10.
+//  Copyright © 2017年 com.xianzaishi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
